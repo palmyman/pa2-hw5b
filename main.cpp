2b8e05ddef0bb49d4ad059f6313251239cbbeb20
Lubomir Cuhel <cuhel.lubomir@gmail.com>
2015-05-07 21:31:33 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: palmyman
+ *
+ * Created on May 7, 2015, 9:30 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
